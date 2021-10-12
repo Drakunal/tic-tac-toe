5a50dcad816ce829c186b0f45f0d1bfa407ac9f3
Drakunal <drakunaldey1@gmail.com>
2021-10-12 18:26:00 +0530

    utils added and player class created

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  static const none = '';
+  static const X = 'X';
+  static const O = 'O';
+}
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -8,8 +14,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  static final countMatrix = 3;
+  static final double size = 92;
+  late List<List<String>> matrix;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setEmptyFields();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tic tac toe"),
+      ),
+      backgroundColor: Colors.blue,
+    );
   }
+
+  void setEmptyFields() => setState(() {
+        matrix = List.generate(countMatrix,
+            (index) => List.generate(countMatrix, (index) => Player.none));
+      });
 }

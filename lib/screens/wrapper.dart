563f7782ec06ae5696867662f4f5c34996a2da35
Drakunal <drakunaldey1@gmail.com>
2021-10-13 21:51:40 +0530

    wrapper created

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,36 @@
+import 'package:debit_credit/screens/home.dart';
+import 'package:debit_credit/screens/signin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/screens/home.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error Occured!"),
+                );
+              } else if (snapshot.hasData) {
+                return Home();
+              } else {
+                return SignIn();
+              }
+            }));
+  }
+}

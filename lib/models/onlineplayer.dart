29fb52f01244cb8120b469829ef048d5c53b95aa
Drakunal <drakunaldey1@gmail.com>
2021-10-13 22:18:22 +0530

    online player model created

---
+++ lib/models/onlineplayer.dart
@@ -0,0 +1,30 @@
+class OnlinePlayer {
+  String uid;
+  String gameId;
+  int numberOfSquars;
+  int m0;
+  int m1;
+  int m2;
+  int m3;
+  int m4;
+  int m5;
+  int m6;
+  int m7;
+  int m8;
+  int m9;
+
+  OnlinePlayer(
+      {required this.uid,
+      required this.gameId,
+      required this.numberOfSquars,
+      required this.m0,
+      required this.m1,
+      required this.m2,
+      required this.m3,
+      required this.m4,
+      required this.m5,
+      required this.m6,
+      required this.m7,
+      required this.m8,
+      required this.m9});
+}
